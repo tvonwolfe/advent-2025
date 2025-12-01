0ad5fd24e07489c5ab4dff418c2c9b1df4bbaccc
Tony Von Wolfe <tonyvonwolfe@protonmail.com>
2025-11-30 20:50:28 -0700

    setup

---
+++ runner.rb
@@ -0,0 +1,20 @@
+class Runner
+  def self.run(*)
+    result = new(*).run
+
+    puts "#{name} Result: #{result}"
+  end
+
+  # @param input [Array<String>]
+  def initialize(input)
+    @input = input
+  end
+
+  def run
+    raise NoMethodError, "Must define #{__method__}!"
+  end
+
+  private
+
+  attr_reader :input
+end
