7ee1f606490c9f1f7bbf1539d2b58b604b91e6fa
Tony Von Wolfe <tonyvonwolfe@protonmail.com>
2025-12-04 19:39:43 -0700

    day 3 part 2, set up day 4

--- day03/part2.rb
+++ day03/part2.rb
@@ -3,7 +3,33 @@
 require_relative "../runner"
 
 class Part2 < Runner
+  def biggest_joltage(bank)
+    accumulator = []
+
+    i = 0
+    while (accumulator.size < 12)
+      end_possible_range = bank.size - (12 - (accumulator.size)) + 1
+      largest = i
+
+      while (i < end_possible_range)
+        if bank[i] > bank[largest]
+          largest = i
+        end
+
+        i += 1
+      end
+
+      accumulator << bank[largest]
+      i = largest + 1
+    end
+
+    accumulator.join.to_i
+  end
+
   def run
-    # do stuff, return result
+    input.reduce(0) do |agg, bank|
+      result = biggest_joltage(bank)
+      agg + result
+    end
   end
 end
