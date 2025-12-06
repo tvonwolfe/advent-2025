6807a8ea9a024dbac006f4ab8bb0b720403e5c94
Tony Von Wolfe <tonyvonwolfe@protonmail.com>
2025-12-05 19:33:10 -0700

    day 4 initial setup

--- day04/part1.rb
+++ day04/part1.rb
@@ -3,7 +3,31 @@
 require_relative "../runner"
 
 class Part1 < Runner
+  class Grid
+    Point = Data.define(:x, :y, :occupied) do
+      def occupied? = !!occupied
+    end
+
+    attr_reader :rows
+
+    def initialize(rows)
+      @rows = rows
+    end
+
+    def accessible_points
+
+    end
+  end
+
   def run
-    # do stuff, return result
+    rows = input.map.with_index do |line, y|
+      line.chars.map.with_index do |char, x|
+        Grid::Point.new(x:, y:, occupied: char == "@")
+      end
+    end
+
+    grid = Grid.new(rows)
+
+    grid.accessible_points.length
   end
 end
