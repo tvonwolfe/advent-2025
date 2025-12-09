82c146e7be4ace15131fb84faede55d9864bfb8f
Tony Von Wolfe <tonyvonwolfe@protonmail.com>
2025-12-08 19:31:12 -0700

    day 7 part 1

--- day07/part1.rb
+++ day07/part1.rb
@@ -3,7 +3,36 @@
 require_relative "../runner"
 
 class Part1 < Runner
+  SPLITTER = "^"
+  TACHYON = "|"
+
   def run
-    # do stuff, return result
+    input.first.gsub!("S", TACHYON)
+    result = []
+    num_splits = 0
+
+    input.each_with_index do |current_line, index|
+      next result << current_line if index.zero?
+
+      previous_line = result[index - 1]
+      result_line = current_line.clone
+
+      previous_line.chars.each_with_index do |char_above, char_index|
+        next unless char_above == TACHYON
+
+        if current_line[char_index] == SPLITTER
+          result_line[char_index - 1] = TACHYON
+          result_line[char_index] = SPLITTER
+          result_line[char_index + 1] = TACHYON
+          num_splits += 1
+        else
+          result_line[char_index] = TACHYON
+        end
+      end
+
+      result << result_line
+    end
+
+    num_splits
   end
 end
